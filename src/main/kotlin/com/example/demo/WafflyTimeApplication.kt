d8b128f2d5a66b22c6b1e13d7bebd586cb40436a
woong97 <yjwoong97@gmail.com>
2022-12-26 14:10:47 +0900

    async email send 구현

--- src/main/kotlin/com/example/demo/WafflyTimeApplication.kt
+++ src/main/kotlin/com/example/demo/WafflyTimeApplication.kt
@@ -3,6 +3,7 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class WafflyTimeApplication
 
