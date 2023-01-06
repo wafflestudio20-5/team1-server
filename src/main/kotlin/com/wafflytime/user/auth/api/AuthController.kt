1b3232733953fc73f29303ef6fccf568d0f0f3f9
woong97 <yjwoong97@gmail.com>
2023-01-07 02:05:00 +0900

    println 제거

--- src/main/kotlin/com/wafflytime/user/auth/api/AuthController.kt
+++ src/main/kotlin/com/wafflytime/user/auth/api/AuthController.kt
@@ -41,7 +41,6 @@ class AuthController(
     @ExemptAuthentication
     @PostMapping("/api/auth/local/login")
     fun localLogin(@Valid @RequestBody request: LoginRequest) : AuthToken {
-        println("try login")
         return localAuthService.login(request)
     }
 
