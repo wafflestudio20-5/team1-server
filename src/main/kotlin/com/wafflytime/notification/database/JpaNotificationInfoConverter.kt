ad864f76df3de47df21c6737cb3dc70fb81a4b3b
woong97 <yjwoong97@gmail.com>
2023-01-16 17:29:34 +0900

    테슽트 html 페이지 알림확인 api 추가

--- src/main/kotlin/com/wafflytime/notification/database/JpaNotificationInfoConverter.kt
+++ src/main/kotlin/com/wafflytime/notification/database/JpaNotificationInfoConverter.kt
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.wafflytime.board.dto.BoardNotificationInfo
 import com.wafflytime.notification.dto.NotificationInfo
+import com.wafflytime.notification.exception.NotificationInfoNotImplemented
 import com.wafflytime.notification.type.NotificationType
 import com.wafflytime.reply.dto.ReplyNotificationInfo
 import jakarta.persistence.AttributeConverter
 import org.json.JSONObject
 
 
-class JpaNotificationRedirectInfoConverter : AttributeConverter<NotificationInfo, String> {
+class JpaNotificationInfoConverter : AttributeConverter<NotificationInfo, String> {
     private val mapper: ObjectMapper = ObjectMapper()
 
     override fun convertToDatabaseColumn(attribute: NotificationInfo?): String? {
@@ -23,13 +24,11 @@ class JpaNotificationRedirectInfoConverter : AttributeConverter<NotificationInfo
         val notificationType = NotificationType.valueOf(JSONObject(dbData).getString("notificationType"))
 
         if (notificationType == NotificationType.REPLY) {
-            println("reply parse!")
             return mapper.readValue(dbData, object : TypeReference<ReplyNotificationInfo>(){})
         } else if (notificationType == NotificationType.BOARD) {
-            println("board parse!")
             return mapper.readValue(dbData, object : TypeReference<BoardNotificationInfo>(){})
         } else {
-            throw NotImplementedError("$notificationType 에 대해서는 구현되지 않았습니다")
+            throw NotificationInfoNotImplemented
         }
     }
 }
--- src/main/kotlin/com/wafflytime/notification/database/NotificationEntity.kt
+++ src/main/kotlin/com/wafflytime/notification/database/NotificationEntity.kt
@@ -11,8 +11,18 @@ import jakarta.validation.constraints.NotNull
 @Table(name="notification")
 class NotificationEntity(
 
+
+    /**
+     * Reply, Board, 쪽지 등 notification이 필요한 도메인들은 각각 redirect 에 필요한 정보들이 다르다.
+     * 각 domain 별로 어떤 형태의 json이든 db column으로 저장하기 위해 NorificationInfo 같은 interface를 사용하였다.
+     * 하나의 class를 만들고 domain이 추가될 때마다 필드가 추가되는게 귀찮고, reply에서 필요한 정보들이 쪽지에서는 다 null로 저장시키기 때문에
+        이를 피하고자 interface를 사용하였는데, 오히려 불편한 면도 있다.
+     * JpaNotificationRedirectInfoConverter 구현을 살펴보면, interface는 deserialize가 안되기 때문에 NotificationType에
+        따라서 다르게 TypeReference하고 있다
+     * interface를 사용하는게 오히려 비효율적이라 판단되면, entity column에 replyInfo, boardInfo, 쪽지Info 각각 가지도록 만들어도 될 것 같다
+     */
     @Column(length = 100, columnDefinition = "json")
-    @Convert(converter = JpaNotificationRedirectInfoConverter::class)
+    @Convert(converter = JpaNotificationInfoConverter::class)
     var info: NotificationInfo? = null,
 
     @ManyToOne(fetch = FetchType.EAGER)
