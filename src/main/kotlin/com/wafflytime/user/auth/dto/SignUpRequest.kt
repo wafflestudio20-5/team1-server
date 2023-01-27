e95a4613257aaba074776794167987971c97460e
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-01-27 22:44:20 +0900

    feat: 닉네임 길이 제한

--- src/main/kotlin/com/wafflytime/user/auth/dto/SignUpRequest.kt
+++ src/main/kotlin/com/wafflytime/user/auth/dto/SignUpRequest.kt
@@ -1,6 +1,7 @@
 package com.wafflytime.user.auth.dto
 
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
 
 data class SignUpRequest(
     @field:NotBlank
@@ -8,5 +9,6 @@ data class SignUpRequest(
     @field:NotBlank
     val password: String,
     @field:NotBlank
+    @field:Length(min = 2, max = 10)
     val nickname: String,
 )
