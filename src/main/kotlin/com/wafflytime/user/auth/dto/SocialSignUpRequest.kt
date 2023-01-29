718cda2092e8f86bd25c55cea51a99bb668b60b2
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-01-30 01:18:50 +0900

    fix: nickname 길이 제한(2자 ~ 10자)

--- src/main/kotlin/com/wafflytime/user/auth/dto/SocialSignUpRequest.kt
+++ src/main/kotlin/com/wafflytime/user/auth/dto/SocialSignUpRequest.kt
@@ -1,8 +1,10 @@
 package com.wafflytime.user.auth.dto
 
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
 
 data class SocialSignUpRequest(
     @field:NotBlank
+    @field:Length(min = 2, max = 10)
     val nickname: String,
 )
